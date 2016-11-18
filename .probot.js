76d09f1fb65c64c85d16cc9da129b6e10271f957
Brandon Keepers <bkeepers@github.com>
2016-11-18 08:58:16 -0600

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,8 +1,8 @@
 on('issues.opened')
   .filter((event) => {
       const body = event.payload.issue.body;
-      return body.includes('- [ ]')
-        || !body.includes('- [x]')
+      return body.includes('* [ ]')
+        || !body.includes('* [x]')
         || !body.match(/### Steps to Reproduce/);
     })
   .comment('Hey @{{ sender.login }}, thanks for opening an issue, but you must fill out the template.')

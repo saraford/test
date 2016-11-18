851f32762ee7cfeb4426b82a67894e2c6ee17fca
Brandon Keepers <bkeepers@github.com>
2016-11-18 08:56:47 -0600

    Update .probot.js

--- .probot.js
+++ .probot.js
@@ -1,14 +1,10 @@
-on("issues.opened")
-  .filter(event => !event.payload.issue.body.includes("- [x]"))
-  .comment("You did not fill out the issue template")
+on('issues.opened')
+  .filter((event) => {
+      const body = event.payload.issue.body;
+      return body.includes('- [ ]')
+        || !body.includes('- [x]')
+        || !body.match(/### Steps to Reproduce/);
+    })
+  .comment('Hey @{{ sender.login }}, thanks for opening an issue, but you must fill out the template.')
+  .label('insufficient-info')
   .close();
-
-on("issues.opened")
-  .filter(event => event.payload.issue.body.includes("- [x]"))
-  .comment("Hello, @{{ sender.login }}! Thanks for opening an issue. This is a demo of PRobot, a trainable robot that responds to activity on GitHub. Check out the config in [.probot](https://github.com/bkeepers/test/blob/master/.probot) and see http://github.com/bkeepers/probot for more information.")
-  .label("feedback");
-
-// Add WIP label if title contains WIP
-on("pull_request.opened")
-  .filter(event => event.payload.pull_request.title.includes("WIP"))
-  .label("wip");
